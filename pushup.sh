4359c6df0d939c9718a6095badf2aedde21074a8
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-02-28 12:15:24 +0530

    read text for csv file

--- pushup.sh
+++ pushup.sh
@@ -21,3 +21,13 @@ while read myVar
 do 
   echo "Value  from file  is $myVar"
 done < names.txt
+
+# read csv file data with while loop
+
+while  IFS="," read  id name age  #IFS(Internal Field Separator in Bash.You can change how Bash splits values) it  tells where to break 
+do 
+  echo "id is $id"
+  echo "name is $name"
+  echo "age is $age"
+done  < test.csv
+
