5045362db73d0b215d7cfd45fc61c4e1c32b831d
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-02-28 16:44:17 +0530

    add

--- for_loop_ping_domains.sh
+++ for_loop_ping_domains.sh
@@ -1,8 +1,9 @@
+
 #!/bin/bash
 
 for x in google.com bing.com facebook.com networkchuck.com;
 do  
-   if ping -q -c 2 -W 1 $x > /dev/null; then
+   if ping -q -c 2 -W 1 $x > /dev/null; 
      echo  "$x is up"
    else 
         echo "$x is down"
