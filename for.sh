35acebf6c0fd07f9450845ca824a68ccacd5f8c3
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-02-28 09:31:27 +0530

    add new part

--- for.sh
+++ for.sh
@@ -1,4 +1,5 @@
 
+
 #!/bin/bash
 
 for cups in 1 2 3 4 5 6 7 8 9 10;
@@ -10,3 +11,21 @@ for cups in {1..10};
 do 
   echo "hey,you've had $cups cups of coffee today"
 done
+
+read -p "Enter your marks: "  marks
+if [[ $marks -gt 40 ]]; then 
+     echo "you are pass"
+else 
+  echo  "you are  fail"
+fi
+
+
+if [[ $marks -ge 80  ]]; then  
+  echo "First  Division"
+elif [[ $marks -ge  60 ]]; then 
+  echo "second Division"
+elif [[ $marks -ge 40 ]]; then
+    echo "three Division"
+else 
+    echo "Fail"
+fi
